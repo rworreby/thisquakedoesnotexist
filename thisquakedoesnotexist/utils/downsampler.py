c9cf5b332a10ec5c00a178e9120462c393b95675
Robin Worreby <robin.worreby@math.ethz.ch>
2023-03-21 12:55:04 +0000

    Add pga computation and file saving to downsampler

--- thisquakedoesnotexist/utils/downsampler.py
+++ thisquakedoesnotexist/utils/downsampler.py
@@ -1,5 +1,6 @@
 import h5py
 import numpy as np
+import pandas as pd
 
 
 class Downsampler:
@@ -9,7 +10,7 @@ class Downsampler:
     If the dimension is 3, downsampling is done on the second dimension.
     Otherwise, no downsampling is performed.
 
-     :param filename: Datafile name to read from
+    :param filename: Datafile name to read from
     :type filename: str
     :param outfile: Datafile name where the downsampled data is written to
     :type outfile: str
@@ -28,6 +29,8 @@ class Downsampler:
         burnin: float,
         duration: float,
         sample_rate: float,
+        waveforms_file: str = '../data/japan/waveforms.npy',
+        attributes_file: str = '../data/japan/attributes.csv',
     ):
         self.filename = filename
         self.outfile = outfile
@@ -36,6 +39,8 @@ class Downsampler:
         self.burnin = burnin
         self.duration = duration
         self.sample_rate = sample_rate
+        self.waveforms_file = waveforms_file
+        self.attributes_file = attributes_file
 
     def __repr__(self) -> str:
         return f"Downsampler instance for file: {self.filename}"
@@ -68,12 +73,50 @@ class Downsampler:
                 self.data[val] = self.h5_file[val][:, threshold_start:]
             elif dimension == 3:
                 self.data[val] = \
-                    self.h5_file[val][:, begin:end:factor, threshold_start:]
+                    np.array(self.h5_file[val][:, begin:end:factor, threshold_start:]).transpose((2, 0, 1))
             else:
                 print(f"Nothing to be done for {val}, skipping downsampling.")
                 self.data[val] = self.h5_file[val][:, :]
                 continue
 
             print(f"Downsampling for {val}:")
-            print(f"Old dimensions of file: {self.h5_file[val].shape}")
-            print(f"New dimension of file: {self.data[val].shape}")
+            print(f"Old dimensions of data: {self.h5_file[val].shape}")
+            print(f"New dimension of data: {self.data[val].shape}")
+    
+    
+    def compute_pga(self):
+        wfs_p = np.abs(self.data['waveforms'])
+        
+        # Calculate the max amplitude for each trace
+        wa = np.max(wfs_p, axis=2)
+        pga_v = np.max(wa, axis=1)
+        
+        self.data['pga_v'] = pga_v.reshape(1, -1)
+        print(self.data.keys())
+
+
+    def save_to_files(self):
+        n_obs = np.array(self.data['waveforms']).shape[0]
+        
+        print(self.data.keys())
+        for key in self.data.keys():
+            print(self.data[key].shape)
+        
+        df = pd.DataFrame({
+            'i_wf': np.arange(n_obs), # index for waveform
+            'dist': self.data['hypocentral_distance'][0, :],  
+            'ev_dep': self.data['hypocentre_depth'][0, :],
+            'hypocentre_latitude': self.data['hypocentre_latitude'][0, :],
+            'hypocentre_longitude': self.data['hypocentre_longitude'][0, :],
+            'is_shallow_crustal': self.data['is_shallow_crustal'][0, :],
+            'log10snr': self.data['log10snr'][0, :],
+            'mag': self.data['magnitude'][0, :],
+            'vs30': self.data['vs30'][0, :],
+            'pga_v': self.data['pga_v'][0, :],
+        })
+
+        df.to_csv(self.attributes_file, index=False)
+
+        np.save(self.waveforms_file, self.data['waveforms'])
+
+        

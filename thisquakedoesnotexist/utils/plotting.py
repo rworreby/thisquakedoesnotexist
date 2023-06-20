7292a6933023cc68078863c337f912cbae9f5e64
Robin Worreby <robin.worreby@math.ethz.ch>
2023-06-20 10:54:48 +0000

    Add rolling max plot to evaluation

--- thisquakedoesnotexist/utils/plotting.py
+++ thisquakedoesnotexist/utils/plotting.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 
 import numpy as np
@@ -21,7 +23,7 @@ def plot_syn_data_single(samples, dist, dt, lt, fig_dir):
         log_signal = np.log(np.abs(np.array(samples[j])))
         signal = np.array(samples[j])
 
-        fig, ax = plt.subplots(3, 1, figsize=(15, 15))
+        fig, ax = plt.subplots(4, 1, figsize=(15, 15))
 
         ax[0].plot(tt, signal, lw=0.5, label='Signal')
         # ax[0].plot(tt, signal_clean, color='r', lw=1, label='Clean Signal')
@@ -32,32 +34,49 @@ def plot_syn_data_single(samples, dist, dt, lt, fig_dir):
 
         window_length = 40
         half_wl = int(window_length / 2)
-        conv_data = np.convolve(log_signal, np.ones(window_length), 'valid') / window_length
+        mean_data = np.convolve(log_signal, np.ones(window_length), 'valid') / window_length
+        
+        rw = rolling_window(log_signal, window_length)
+
+        rolling_median = [None] * half_wl
+        for i, window in enumerate(rw):
+            rolling_median.append(np.median(window))
 
         ax[1].plot(tt, log_signal, lw=0.5, label='Log Transformed Signal')
-        ax[1].plot(tt[half_wl:-half_wl+1], conv_data, label='Centered Rolling Mean')
+        ax[1].plot(tt[half_wl:-half_wl+1], mean_data, label='Centered Rolling Mean')
+        ax[1].plot(tt[:-half_wl], rolling_median, label='Centered Rolling Median')
         ax[1].set_xlabel('Time [s]')
         ax[1].set_ylabel('Log Amplitude')
         ax[1].legend()
 
+        rolling_max = [None] * half_wl
+        rw = rolling_window(log_signal, window_length)
+
+        for i, window in enumerate(rw):
+            rolling_max.append(window.max())
+
+        ax[2].plot(tt, log_signal, lw=0.5, label='Log Transformed Signal')
+        ax[2].plot(tt[half_wl:], rolling_max, label='Centered Rolling Max')
+        ax[2].set_xlabel('Time [s]')
+        ax[2].set_ylabel('Log Amplitude')
+        ax[2].legend()
+
         s_fft = np.fft.rfft(signal)
         s_ps = np.real(s_fft * np.conj(s_fft))
-        
         freq = np.fft.rfftfreq(signal.size, d=dt)
 
         rw = rolling_window(s_ps, window_length)
-
         rolling_mean = [None] * half_wl
 
         for i, window in enumerate(rw):
             rolling_mean.append(window.mean())
 
-        ax[2].loglog(freq, s_ps, lw=0.5, label='Fourier Transformed Signal')
-        ax[2].plot(freq[:-half_wl], rolling_mean, label='Centered Rolling Mean')
-        ax[2].set_xlabel('Frequenciy [Hz]')
-        ax[2].set_ylabel('Fourier Amplitude')
-        ax[2].set_xlim((0.1, 50))
-        ax[2].legend()
+        ax[3].loglog(freq, s_ps, lw=0.5, label='Fourier Transformed Signal')
+        ax[3].plot(freq[:-half_wl], rolling_mean, label='Centered Rolling Mean')
+        ax[3].set_xlabel('Frequency [Hz]')
+        ax[3].set_ylabel('Fourier Amplitude')
+        ax[3].set_xlim((0.1, 50))
+        ax[3].legend()
 
         fig.suptitle(f'Random Synthetic Waveform. Dist: {dist}')
         plt.subplots_adjust(hspace=0.4)
@@ -96,6 +115,7 @@ def plot_syn_data_grid(samples, distance, dt, fig_dir):
     fig.suptitle(f'Randomly drawn samples from Generator. Distance: {distance} km')
     fig_file = os.path.join(fig_dir, 'syntetic_data_grid_plot.png')
     plt.savefig(fig_file, format='png')
+
 
 def plot_waves_1C(ws, dt, t_max=50.0, ylim=None, fig_file=None, Np=12, fig_size = (6,9), stitle=None, show_fig=False):
     """

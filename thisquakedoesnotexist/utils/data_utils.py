2be6891b2cd17bf087f70794b985f2b042d66200
Robin Worreby <robin.worreby@math.ethz.ch>
2023-08-02 11:53:02 +0000

    Refactor functions from main to data utils

--- thisquakedoesnotexist/utils/data_utils.py
+++ thisquakedoesnotexist/utils/data_utils.py
@@ -1,54 +1,58 @@
-# %%
+#!/usr/bin/env python3
+
+import os
+import shutil
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-# from mtspec import mtspec
 
 
-def plot_waves_3C(ws, dt, t_max=20.0, Np=10, color='C0', chan='chan_first'):
+def set_up_folders(run_id, args):
     """
-        Make plot of waves
-        shape of the data has the form -> (Nobs, 3, Nt)
-        :param ws_p: numpy.ndarray of shape (Nobs, 3, Nt)
-        :param Np: number of 3C waves to show
-        :param color: Color of the plot
-        :param t_max: max time for the plot in sec
-        :param chan: channel type, default chan_first
-        :return: show a panel with the waves
+    Output configurations dictionary and create data directory
+    """
+    output_dir = args.output_dir + "_" + str(run_id)
+    model_file = args.model_file
 
-        """
-    Nt = int(t_max / dt)
-    if ws.shape[0] < Np:
-        # in case there are less observations than points to plot
-        Np = ws.shape[0]
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    if chan == 'chan_first':
-        ws_p = ws[:Np, :, :Nt]
-    elif chan == 'chan_last':
-        # change format to default channel first
-        ws_p = np.transpose(ws, (0, 2, 1))
-        ws_p = ws_p[:Np, :, :Nt]
-    else:
-        assert False, "Undefined channel type, it must be either chan_first or chan_last "
+    # create diretory for models
+    models_dir = os.path.join(output_dir, "models")
+    if not os.path.exists(models_dir):
+        os.makedirs(models_dir)
 
-    # select waves to plot
-    tt = dt * np.arange(Nt)
-    plt.figure()
-    fig, axs = plt.subplots(Np, 3, sharex='col', sharey='row',
-                            gridspec_kw={'hspace': 0, 'wspace': 0},
-                            figsize=(14, 10))
-    for i in range(Np):
-        for j in range(3):
-            wf = ws_p[i, j, :]
-            if Np > 1:
-                axs[i, j].plot(tt, wf, color=color)
-            else:
-                axs[j].plot(tt, wf, color=color)
+    # create directory for figures
+    training_dir = os.path.join(output_dir, "training_plots")
+    if not os.path.exists(training_dir):
+        os.makedirs(training_dir)
 
-    for ax in axs.flat:
-        ax.label_outer()
-        plt.show()
+    metrics_dir = os.path.join(output_dir, "metrics")
+    if not os.path.exists(metrics_dir):
+        os.makedirs(metrics_dir)
 
+    grid_dir = os.path.join(output_dir, "grid_plots")
+    if not os.path.exists(grid_dir):
+        os.makedirs(grid_dir)
+
+    fig_dir = os.path.join(output_dir, "figs")
+    if not os.path.exists(fig_dir):
+        os.makedirs(fig_dir)
+
+    # make a copy of gan.py which contains the model specifications
+    model_file_name = model_file.split("/")[-1]
+    gan_out = os.path.join(output_dir, model_file_name)
+    shutil.copyfile(model_file, gan_out)
+    dirs = {
+        "output_dir": output_dir,
+        "models_dir": models_dir,
+        "training_dir": training_dir,
+        "fig_dir": fig_dir,
+        "metrics_dir": metrics_dir,
+        "grid_dir": grid_dir,
+    }
+    return dirs
 
 
 # rescale conditional variables
@@ -58,12 +62,13 @@ def rescale(v):
     """
     v_min = v.min()
     v_max = v.max()
-    dv = v_max-v_min
+    dv = v_max - v_min
     # rescale to [0,1]
-    vn = (v-v_min) / dv
+    vn = (v - v_min) / dv
     # rescale to [-1,1]
-    #vn = 2.0 * vn - 1.0
+    # vn = 2.0 * vn - 1.0
     return vn
+
 
 # factory function to rescale variables
 def make_maps_scale(v_min, v_max):
@@ -72,16 +77,18 @@ def make_maps_scale(v_min, v_max):
     """
 
     def to_scale_11(x):
-        dv = v_max-v_min
-        xn = (x-v_min) / dv
+        dv = v_max - v_min
+        xn = (x - v_min) / dv
         # rescale to [-1,1]
-        xn = 2.0*xn -1.0
+        xn = 2.0 * xn - 1.0
         return xn
+
     def to_real(x):
-        dv = v_max-v_min
-        xr = (x+1.0)/2.0
-        xr = xr*dv+v_min
+        dv = v_max - v_min
+        xr = (x + 1.0) / 2.0
+        xr = xr * dv + v_min
         return xr
+
     # return tuple of functions
     return (to_scale_11, to_real)
 
@@ -90,6 +97,7 @@ class SeisData(object):
     """
     Class to manage seismic data
     """
+
     # ------- define key class varibles ----------------
     # numpy array (Nobs, 3 , Nt) normalized waveforms
     wfs = None
@@ -97,7 +105,6 @@ class SeisData(object):
     ws = None
     # normalization factors (Nobs, 3 )
     cnorms = None
-
 
     # list with pseudo continuous conditional variables scaled to [0,1)
     vc_lst = None
@@ -136,16 +143,16 @@ class SeisData(object):
 
         self.v_names = v_names
         # load data
-        print('Loading data ...')
+        print("Loading data ...")
         wfs = np.load(data_file)
-        print('Loaded samples: ', wfs.shape[0])
+        print("Loaded samples: ", wfs.shape[0])
         # total number of training samples
         Ntrain = wfs.shape[0]
         self.Ntrain = Ntrain
         # store non normalized waveforms
         self.ws = wfs.copy()
-        print('normalizing data ...')
-        wfs_norm = np.max(np.abs(wfs), axis=1) #2)
+        print("normalizing data ...")
+        wfs_norm = np.max(np.abs(wfs), axis=1)  # 2)
         # store normalization facots
         cnorms = wfs_norm.copy()
         self.cnorms = cnorms
@@ -159,16 +166,16 @@ class SeisData(object):
         vns_min = np.min(cnorms, axis=0)
         pga_max = vns_max.max()
         pga_min = vns_min.min()
-        log_pga_max = np.log10( pga_max )
-        log_pga_min = np.log10( pga_min )
+        log_pga_max = np.log10(pga_max)
+        log_pga_min = np.log10(pga_min)
         lc_m = np.log10(cnorms)
-        fn_to_scale_11, fn_to_real = make_maps_scale( log_pga_min, log_pga_max)
+        fn_to_scale_11, fn_to_real = make_maps_scale(log_pga_min, log_pga_max)
         # create array for rescaled varibles
         ln_cns = np.zeros_like(lc_m)
 
         ln_cns[:] = fn_to_scale_11(lc_m[:])
-        #ln_cns[:,1] = fn_to_scale_11(lc_m[:,1])
-        #ln_cns[:,2] = fn_to_scale_11(lc_m[:,2])
+        # ln_cns[:,1] = fn_to_scale_11(lc_m[:,1])
+        # ln_cns[:,2] = fn_to_scale_11(lc_m[:,2])
         # store variables of interest
         self.fn_to_scale_11 = fn_to_scale_11
         self.fn_to_real = fn_to_real
@@ -184,7 +191,6 @@ class SeisData(object):
         self.df_meta_all = df.copy()
         # store pandas dict as attribute
         self.df_meta = df[self.v_names]
-
 
         # ----- Configure conditional variables --------------
         # store normalization constant for conditional variables
@@ -214,19 +220,19 @@ class SeisData(object):
         self.vc = np.hstack(self.vc_lst)
 
         self.Ntrain = Nsel
-        print('Number selected samples: ', Nsel)
+        print("Number selected samples: ", Nsel)
 
-        print('Class init done!')
+        print("Class init done!")
 
     def to_real(self, vn, v_name):
         # Rescale variable to its original range
         v_min = self.vc_min[v_name]
         v_max = self.vc_max[v_name]
-        dv = v_max-v_min
+        dv = v_max - v_min
         # rescale to [v_min, v_max]
-        v = vn*dv + v_min
+        v = vn * dv + v_min
         # rescale to [-1,1]
-        #vn = 2.0 * vn - 1.0
+        # vn = 2.0 * vn - 1.0
         return v
 
     def to_syn(self, vr, v_name):
@@ -234,11 +240,10 @@ class SeisData(object):
         # synthetic [0, 1] range
         v_min = self.vc_min[v_name]
         v_max = self.vc_max[v_name]
-        dv = v_max-v_min
+        dv = v_max - v_min
         # rescale to [0,1]
-        vn = (vr-v_min) / dv
+        vn = (vr - v_min) / dv
         return vn
-
 
     def _set_vc_max(self):
         """
@@ -261,18 +266,17 @@ class SeisData(object):
 
         self.vc_lst = []
         for v_name in self.v_names:
-            print('---------', v_name, '-----------')
-            print('min ' + v_name, self.df_meta[v_name].min())
-            print('max ' + v_name, self.df_meta[v_name].max())
+            print("---------", v_name, "-----------")
+            print("min " + v_name, self.df_meta[v_name].min())
+            print("max " + v_name, self.df_meta[v_name].max())
             # 1. rescale variables to be between 0,1
             v = rescale(self.df_meta[v_name].to_numpy())
             # reshape conditional variables
             vc = np.reshape(v, (v.shape[0], 1))
-            print('vc shape', vc.shape)
+            print("vc shape", vc.shape)
             # 3. store conditional variable
             self.vc_lst.append(vc)
         ## end method
-
 
     def _get_rand_idxs(self):
         """
@@ -321,7 +325,7 @@ class SeisData(object):
         return self.batch_size
 
     def __str__(self):
-        return 'wfs data shape: ' + str(self.wfs.shape)
+        return "wfs data shape: " + str(self.wfs.shape)
 
     def get_Ntrain(self):
         """
@@ -337,37 +341,3 @@ class SeisData(object):
         """
         Nb_tot = np.floor(self.Ntrain / self.batch_size)
         return int(Nb_tot)
-
-    def plot_waves_rand_3C(self, t_max=20.0, Np=10, color='C0', ):
-        """
-        Make plot of waves
-        shape of the data has the form -> (Nobs, 3, Nt)
-        :param t_max: max time for the plot in sec
-        :param Np: number of 3C waves to show
-        :param color: Color of the plot
-        :return: show a panel with the waves
-        """
-        dt = self.dt
-        # get a batch of waves at random
-        (ws, _) = self.get_rand_batch()
-        plot_waves_3C(ws, dt, t_max, Np=Np, color=color)
-
-# %%
-# # ---- Testing ----
-# data_file = '/Users/manuel/seisML/raw_data/mini_downsamp_5x_all.npy'
-# attr_file = '/Users/manuel/seisML/raw_data/mini_wforms_table.csv'
-# Nbatch = 32
-# bins_dict = {
-#     'dist': 4,
-#     'mag': [3.0, 4.0, 5.0, 6.0, 'inf'],
-#
-# }
-# v_names = ['dist', 'mag']
-# s_dat = SeisData(data_file, attr_file=attr_file, batch_size=Nbatch, sample_rate=20.0, v_names=v_names, bins_d=bins_dict)
-#
-# print(s_dat._get_rand_idxs())
-#
-# # get a random sample
-# (wfs, i_vc) = s_dat.get_rand_batch()
-# print('shape:', wfs.shape)
-# s_dat.plot_waves_3C(Np=4, color='C1')

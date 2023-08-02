ee0de926ae8069505499add7d41065564ed0d478
Robin Worreby <robin.worreby@math.ethz.ch>
2023-08-02 11:54:05 +0000

    Update args in runfile

--- thisquakedoesnotexist/runfiles/run_amplitudes.sh
+++ thisquakedoesnotexist/runfiles/run_amplitudes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gpus=1
-name="Amplitudes Test"
+experiment_name="Amplitudes"
 batch_size=128
 noise_dim=100
 sample_rate=20
@@ -10,11 +10,13 @@ critic_iter=12
 lr=1e-4
 dt=0.05
 model_file="thisquakedoesnotexist/models/gan.py"
+plot_format='pdf'
 
-for epochs in 1; do
-    python thisquakedoesnotexist/amplitudes/amplitudes.py --gpu "$gpus" \
-        --name "$name" --batch_size "$batch_size" --noise_dim "$noise_dim" \
+for epochs in 200; do
+    python thisquakedoesnotexist/amplitudes/amplitudes.py --gpus "$gpus" \
+        --experiment_name "$experiment_name" --batch_size "$batch_size" --noise_dim "$noise_dim" \
         --sample_rate "$sample_rate" --frac_train "$frac_train" \
-        --n_critic "$critic_iter" --lr "$lr" --epochs "$epochs" \
-        --model_file "$model_file" --dt "$dt"
+        --n_critic "$critic_iter" -lr "$lr" --epochs "$epochs" \
+        --model_file "$model_file" -dt "$dt" --plot_format "$plot_format" \
+        --no_vs30_bins
 done

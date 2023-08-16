4d0a3f4f4746af3156ff6d3b98a95079ef0cf8f8
Robin Worreby <robin.worreby@math.ethz.ch>
2023-08-16 08:45:50 +0000

    Update amplitudes runfile

--- thisquakedoesnotexist/runfiles/run_amplitudes.sh
+++ thisquakedoesnotexist/runfiles/run_amplitudes.sh
@@ -7,12 +7,12 @@ noise_dim=100
 sample_rate=20
 frac_train=0.9
 critic_iter=12
-lr=1e-4
+lr=1e-5
 dt=0.05
 model_file="thisquakedoesnotexist/models/gan.py"
 plot_format='pdf'
 
-for epochs in 80; do
+for epochs in 200; do
     python thisquakedoesnotexist/amplitudes/amplitudes.py --gpus "$gpus" \
         --experiment_name "$experiment_name" --batch_size "$batch_size" \
         --noise_dim "$noise_dim" --sample_rate "$sample_rate" \

2ce4cc8ccc17ffbbc4d4b1d52bb61c2c9e9464e3
Robin Worreby <robin.worreby@math.ethz.ch>
2023-10-15 15:23:26 +0000

    Update demo bash script all params

--- thisquakedoesnotexist/runfiles/run_all_params.sh
+++ thisquakedoesnotexist/runfiles/run_all_params.sh
@@ -14,8 +14,8 @@ gp_lambda=10
 beta1=0.0
 beta2=0.9
 sample_rate=20
-n_cond_bins=20
-epochs=2
+n_cond_bins=10
+epochs=80
 batch_size=128
 frac_train=0.9
 output_dir="thisquakedoesnotexist/data/output"
@@ -26,16 +26,14 @@ tracking_uri="/home/rworreby/thisquakedoesnotexist/mlruns/"
 experiment_name="Unnamed Experiment"
 
 
-for epochs in 2; do
-    python thisquakedoesnotexist/amplitudes/amplitudes.py \
-        --gpus "$gpus" --model_name "$model_name" --data_file "$data_file" \
-        --attr_file "$attr_file" --model_file "$model_file" -lt "$lt" \
-        -dt "$dt" --noise_dim "$noise_dim" --learning_rate "$learning_rate" \
-        --n_critic "$n_critic" --gp_lambda "$gp_lambda" --beta1 "$beta1" \
-        --beta2 "$beta2" --sample_rate "$sample_rate" \
-        --n_cond_bins "$n_cond_bins" --epochs "$epochs" \
-        --batch_size "$batch_size" --frac_train "$frac_train" \
-        --output_dir "$output_dir" --print_freq "$print_freq" \
-        --save_freq "$save_freq" --plot_format "$plot_format" \
-        --tracking_uri "$tracking_uri" --experiment_name "$experiment_name"
-done
+python thisquakedoesnotexist/amplitudes/amplitudes.py \
+    --gpus "$gpus" --model_name "$model_name" --data_file "$data_file" \
+    --attr_file "$attr_file" --model_file "$model_file" -lt "$lt" \
+    -dt "$dt" --noise_dim "$noise_dim" --learning_rate "$learning_rate" \
+    --n_critic "$n_critic" --gp_lambda "$gp_lambda" --beta1 "$beta1" \
+    --beta2 "$beta2" --sample_rate "$sample_rate" \
+    --n_cond_bins "$n_cond_bins" --nvb --epochs "$epochs" \
+    --batch_size "$batch_size" --frac_train "$frac_train" \
+    --output_dir "$output_dir" --print_freq "$print_freq" \
+    --save_freq "$save_freq" --plot_format "$plot_format" \
+    --tracking_uri "$tracking_uri" --experiment_name "$experiment_name"

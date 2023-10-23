e088a529edbafdc7781d5b6685cdc2cd9603a02a
Robin Worreby <robin.worreby@math.ethz.ch>
2023-10-23 23:35:49 +0000

    Update repo config files

--- HISTORY.rst
+++ HISTORY.rst
@@ -3,6 +3,8 @@ History
 =======
 
 0.1.0 (2023-02-28)
+1.0.0 (2023-10-24)
 ------------------
 
 * First initial project setup.
+* Initial release.
--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.ethz.ch/worreby-msc-thesis/thisquakedoesnotexist',
-    version='0.1.0',
+    version='1.0.0',
     zip_safe=False,
 )
